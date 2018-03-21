2fa202f29556616a2d0cf81e29e0d90b0baae3e8
AntoineCARON <antoinecaron.simplon@gmail.com>
2018-03-21 09:28:47 +0100

    add exercice

--- poo/exercice01.php
+++ poo/exercice01.php
@@ -22,24 +22,48 @@
  * @param[in] $firstName A string containing the first name.
  * @param[in] $lastName A string containing the last name.
  */
-function fullName($firstName, $lastName)
-{
-    echo "<p>".$firstName." ".$lastName."</p>";
-}
+// function fullName($firstName, $lastName)
+// {
+//     echo "<p>".$firstName." ".$lastName."</p>";
+// }
 
-$actors = [
-    [ 'firstName' => 'John',        'lastName' => 'Gielgud'],
-    [ 'firstName' => 'Laurence',    'lastName' => 'Olivier'],
-    [ 'firstName' => 'Vivien',      'lastName' => 'Leigh'],
-];
+// $actors = [
+//     [ 'firstName' => 'John',        'lastName' => 'Gielgud'],
+//     [ 'firstName' => 'Laurence',    'lastName' => 'Olivier'],
+//     [ 'firstName' => 'Vivien',      'lastName' => 'Leigh'],
+// ];
 
-echo "<h1>Actors list</h1>";
+// echo "<h1>Actors list</h1>";
 
-foreach ($actors as $actor) {
-    fullName($actor['firstName'], $actor['lastName']);
-}
+// foreach ($actors as $actor) {
+//     fullName($actor['firstName'], $actor['lastName']);
+// }
 
 // vim: set expandtab:
 // vim: set tabstop=4 shiftwidth=4 softtabstop=4:
+
+class Actors{
+    public $_firstName;
+    public $_lastName;
+
+    function fullName(){
+       echo '<p>'.$this->_firstName.' '.$this->_lastName.'</p>';
+    }
+}
+
+$character = [];
+$character[] = new Actors;
+$character[0] ->_firstName = 'John';
+$character[0] ->_lastName = 'Gielgud';
+$character[] = new Actors;
+$character[1] ->_firstName = 'Laurence';
+$character[1] ->_lastName = 'Olivier';
+$character[] = new Actors;
+$character[2] ->_firstName = 'Vivien';
+$character[2] ->_lastName = 'Leigh';
+
+foreach ($character as $actor){
+    $actor->fullName();
+}
 ?>
 
--- poo/exercice1.php
+++ poo/exercice1.php
@@ -21,3 +21,40 @@
 * Créer ensuite 3 chats et 2 chiens, les mettres dans un tableau
 * Parcourir le tableau et afficher soit le crie, la vitesse ou son temps de sieste.
 */
+
+interface Animal{
+    public $cryName;
+    public $speed;
+
+    public function Cry(){
+        return $this->cryName.' '.;
+    }
+    public function Run(){
+        return $this->speed.' '.;
+    }
+    public function construct($newscryName, $newspeed){
+        $this->cryName = $newcryName;
+        $this->speed = $newspeed;
+    }
+}
+
+class Pet implements Animal{
+    public $_dog;
+    public $_cat;
+
+    public function sleep(){
+
+    }
+    public function construct($news_dog, $new_cat){
+        $this->_dog = $new_dog;
+        $this->_cat = $new_cat;
+    }
+}
+
+$SleepTime = [];
+$SleepTime = new Animal;
+
+foreach ($animals as $animal){
+    $actor->fullName();
+}
+?>
