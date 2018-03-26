053b68f508a2fb12b103af442ba5c091e683410c
AntoineCARON <antoinecaron.simplon@gmail.com>
2018-03-26 09:30:28 +0200

    add exercice and correction

--- boucles/exercice3.php
+++ boucles/exercice3.php
@@ -6,14 +6,9 @@
 
 $data = [4,3.2,'thomas','formation',5,3.14,8];
 
-// foreach ($data as $valeur){
-//         echo $valeur.'<br/>'."\n";
-// }
-
-foreach ($data as $data) {
-    echo "is_int(";
-    var_export($data);
-    echo ")=";
-    var_dump(is_int($data));
+foreach ($data as $value) {
+    if (is_int($value)) {
+        echo $value . '<br />';
+    }
 }
-
+ 
--- boucles/exercice4.php
+++ boucles/exercice4.php
@@ -11,3 +11,33 @@
 */
 
 $sentence = "La formation simplon c'est genial, on va devenir des pros";
+
+// Boucle qui lit toute la variable $sentence
+for ($i=0; $i < $sentence; $i++) {
+    
+    // return $sentence;
+    // Indique si une valeur appartient à un tableau
+    $voyelle = array("a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y");    
+        if (in_array($voyelle)) {
+            // Calcule la taille d'une chaîne
+            $str = $sentence;
+            echo strlen($str);
+        }
+}
+
+
+
+
+// Calcule la taille d'une chaîne
+// $str = $sentence;
+// echo strlen($str);
+
+
+
+// $voyelle = array("a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y");
+// if (in_array("Irix", $voyelle)) {
+//     echo "Got Irix";
+// }
+// if (in_array("mac", $voyelle)) {
+//     echo "Got mac";
+// }
